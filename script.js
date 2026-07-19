99eae0e0d5a10083029cf03fb047680bd950fcfd
Sandro Spirig <sandrospirig@gmail.com>
2026-07-19 18:32:06 +0700

    refactor: update basket UI with new button structure, improve responsiveness, and enhance styling

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 function init() {
   renderDishes();
   renderBasket();
+  window.addEventListener("resize", renderBigBasketBtn);
   btnCloseModal.addEventListener("click", () => toggleModal("modal", false));
   overlay.addEventListener("click", () => toggleModal("modal", false));
 }
@@ -26,29 +27,30 @@ function renderDishes() {
   }
 }
 
+/* prettier-ignore */
 function renderBasket() {
   let contentRef = document.getElementById("basket-wrapper");
   let basketData = basket[0];
-
   if (basketData.items.length === 0) {
     contentRef.innerHTML = getEmptyBasketTemplate(basketData);
   } else {
     contentRef.innerHTML = getFullBasketData(basketData);
   }
+  renderBigBasketBtn();
+  if (window.innerWidth < 1200 &&contentRef?.classList.contains("is-visible")) {
+    document.getElementById("close-basket")?.classList.remove("hidden");
+  }
 }
 
+/* prettier-ignore */
 function getFullBasketData(basketData) {
-  let htmlRef = `<div class="content-basket"><h3>${basketData.title}</h3><div class="added-to-basket">`;
+  let htmlRef = `<div class="content-basket"><button id="close-basket" class="hidden" onclick="toggleModal('basket-wrapper', false)">&times;</button><h3>${basketData.title}</h3><div class="added-to-basket">`;
   for (let i = 0; i < basketData.items.length; i++) {
     htmlRef += getBasketTemplate(basketData.items[i], i);
   }
   let sub = calculateSubTotal(basketData.items);
   htmlRef += `</div>`;
-  htmlRef += getFullPriceTemplate(
-    sub,
-    deliveryFee,
-    calculateTotal(calculateSubTotal(basketData.items), deliveryFee),
-  );
+  htmlRef += getFullPriceTemplate(sub,deliveryFee,calculateTotal(sub, deliveryFee),);
   return htmlRef + `</div>`;
 }
 
@@ -57,7 +59,6 @@ function addToBasket(catIndex, dishIndex) {
   let dishWitcount = { ...selectedDish, count: 1 };
   let items = basket[0].items;
   let existingItem = items.find((item) => item.name === selectedDish.name);
-
   if (existingItem) {
     existingItem.count++;
   } else {
@@ -100,15 +101,28 @@ function emptyBasket() {
   renderBasket();
 }
 
+function renderBigBasketBtn() {
+  let bigBasketContainer = document.querySelector(".big-basket-btn-container");
+  bigBasketContainer.innerHTML = bigbasketBtntemplate();
+  bigBasketContainer.classList.toggle("hidden", window.innerWidth > 1200);
+}
+
+function calculateTotalCount() {
+  let items = basket[0].items;
+  let totalCount = 0;
+  for (let i = 0; i < basket[0].items.length; i++) {
+    totalCount += items[i].count;
+  }
+  return totalCount;
+}
+
 /* prettier-ignore */
 function toggleModal(idOfElement, show) {
   if (idOfElement === "basket-wrapper") {
-    let element = document.getElementById(idOfElement);
-    if (element) {
-      element.classList.toggle("is-visible", show);
-      document.getElementById("basket-overlay").classList.toggle("is-visible", show);
-    }
-  } else if (idOfElement === "modal") {
+    document.getElementById(idOfElement)?.classList.toggle("is-visible", show);
+    document.getElementById("basket-overlay")?.classList.toggle("is-visible", show);
+    toggleScroll(show);
+    } else if (idOfElement === "modal") {
     modal.classList.toggle("hidden", !show);
     overlay.classList.toggle("hidden", !show);
     if (show) emptyBasket();
@@ -118,4 +132,14 @@ function toggleModal(idOfElement, show) {
 function clearItem(i) {
   basket[0].items.splice(i, 1);
   renderBasket();
+}
+
+function toggleScroll(show) {
+  if (window.innerWidth < 1200 && show) {
+    document.body.style.overflow = "hidden";
+    document.getElementById("close-basket").classList.remove("hidden");
+  } else {
+    document.body.style.overflow = "auto";
+    document.getElementById("close-basket").classList.add("hidden");
+  }
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -13,13 +13,14 @@ function getDishTemplate(dish, catIndex, dishIndex) {
           <p class= "price" >${dish.price.toFixed(2)}$</p>
           </div>
           <p class= "description">${dish.description}</p>
-          <Button id="btn-atb" onclick="addToBasket(${catIndex}, ${dishIndex})">Add to basket</Button>
+          <button class="btn-atb" onclick="addToBasket(${catIndex}, ${dishIndex})">Add to basket</button>
           </div>
         </div>`;
 }
 
 function getEmptyBasketTemplate(basketData) {
   return `<div class="content-basket">
+        <button id="close-basket" class="hidden" onclick="toggleModal('basket-wrapper', false)">&times;</button>
         <h3>${basketData.title}</h3>
         <p>${basketData.empty}</p>
         <img src=${basketData.img} alt="basket-icon" class="basket-img">
@@ -32,7 +33,7 @@ function getBasketTemplate(item, i) {
   return `<div class="basket-description">
          <div class="name-and-price">
          <span id="basket-name">${item.count}x ${item.name}</span>
-         ${isMultiple ? `<button id="delete-btn" onclick="clearItem(${i})"><img id="delete-btn-img" class="delete-right" src="./assets/icons/delete-btn.png"></button>` : `<span class="basket-price">${itemTotalPrice} €</span>`}
+         ${isMultiple ? `<button class="delete-btn" onclick="clearItem(${i})"><img class="delete-btn-img" class="delete-right" src="./assets/icons/delete-btn.png"></button>` : `<span class="basket-price">${itemTotalPrice} €</span>`}
          </div>
          ${getBasketToolbarTemplate(item, i, isMultiple, itemTotalPrice)}
         </div>`;
@@ -41,9 +42,9 @@ function getBasketTemplate(item, i) {
 function getBasketToolbarTemplate(item, i, isMultiple, itemTotalPrice) {
   return `<div class="basket-toolbar">
          <div class="basket-btns">
-         ${isMultiple ? `<button id="minus-btn" onclick="removeItem(${i})">-</button>` : `<button id="delete-btn" onclick="removeItem(${i})"><img id="delete-btn-img" src="./assets/icons/delete-btn.png"></button>`}
-         <span>${item.count}</span>
-         <button id="plus-btn" onclick="addCount(${i})">+</button>
+         ${isMultiple ? `<button class="minus-btn" onclick="removeItem(${i})">-</button>` : `<button class="delete-btn" onclick="removeItem(${i})"><img class="delete-btn-img" src="./assets/icons/delete-btn.png"></button>`}
+         <span class="count">${item.count}</span>
+         <button class="plus-btn" onclick="addCount(${i})">+</button>
          </div>
          ${isMultiple ? `<span class="basket-price">${itemTotalPrice} €</span>` : ""}
     </div>
@@ -56,4 +57,10 @@ function getFullPriceTemplate(subSum, deliveryFee, totalSum) {
       <div class="delivery-fee"><p>Delivery-fee</p><p>${deliveryFee.toFixed(2)}$</p></div>
       <div class="total"><p>Total</p><p >${totalSum.toFixed(2)}$</p></div>
       <button class="buy-now" onclick="toggleModal('modal',true)">Buy now  ${totalSum.toFixed(2)}$</button></div>`;
+}
+
+function bigbasketBtntemplate() {
+  let total = calculateTotalCount();
+  let hiddenClass = total === 0 ? "hidden" : "";
+  return `<button class="big-basket-btn" onclick="toggleModal('basket-wrapper', !document.getElementById('basket-wrapper').classList.contains('is-visible'))">Basket <span class="item-count ${hiddenClass}">${total}</span></button>`;
 }
